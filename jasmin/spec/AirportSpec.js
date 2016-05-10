64efc53ebfb7589256e09297f1745cf342977bae
gerauf <fo4786@gmail.com>
2016-05-10 16:23:49 +0100

    minimum viable product

--- jasmin/spec/AirportSpec.js
+++ jasmin/spec/AirportSpec.js
@@ -2,43 +2,91 @@ describe ("Airport", function(){
   var airport;
 
   beforeEach(function() {
-    airport = new Airport();
     plane = new Plane();
+    airport = new Airport();
   });
+
   it('should start empty', function(){
+    spyOn(Math, 'random').and.returnValue(0.6);
     expect(airport.planes).toEqual([]);
   })
   it('should keep track of all the landed planes', function () {
+    spyOn(Math, 'random').and.returnValue(0.6);
     airport.land(plane)
     expect(airport.planes).toEqual([plane]);
   })
   it('planes in airport shouldnt be flying', function(){
+    spyOn(Math, 'random').and.returnValue(0.6);
     airport.land(plane);
     expect(plane.flying).toEqual(false)
   })
+
   it("can't land a plane which is on land", function(){
+    spyOn(Math, 'random').and.returnValue(0.6);
     airport.land(plane);
     expect(function() {
       airport.land(plane);
     }).toThrowError('Already landed!')
   })
+
+  describe('when stormy', function(){
+
+    it('should not allow a plane to land', function(){
+      spyOn(Math, 'random').and.returnValue(0.4);
+      expect(function(){
+        airport.land(plane);
+      }).toThrowError('Too stormy!')
+    })
+
+    it('should not allow a plane to take off', function(){
+      spyOn(Math, 'random').and.returnValue(0.4);
+      expect(function(){
+        airport.takeoff(plane);
+      }).toThrowError('Too stormy!')
+    })
+  })
+
+
   describe('#takeoff', function(){
     it('plane no longer in airport', function(){
+      spyOn(Math, 'random').and.returnValue(0.6);
       airport.land(plane);
       airport.takeoff(plane);
       expect(airport.planes).toEqual([])
     });
 
     it('should allow a plane to take off', function(){
+      spyOn(Math, 'random').and.returnValue(0.6);
       airport.land(plane);
       airport.takeoff(plane);
       expect(plane.flying).toEqual(true)
     });
 
-    it('should throw an error if the plane takes of in the air', function(){
+
+    it('should throw an error if the plane takes off in the air', function(){
+      spyOn(Math, 'random').and.returnValue(0.6);
       expect(function(){
         airport.takeoff(plane);
       }).toThrowError('Already flying!')
     });
+  })
+  describe('When the airport is full', function(){
+    it('should throw an error when the airport is full', function(){
+      spyOn(Math, 'random').and.returnValue(0.6);
+      for (var i = 0; i < 10; i++){ airport.land(new Plane()); }
+      expect(function(){
+        airport.land(plane)
+      }).toThrowError("We're packed")
+    })
+
+    it('different airports have different capacities',function(){
+      smallCapacity = 5
+      smallAirport = new Airport(smallCapacity)
+      spyOn(Math, 'random').and.returnValue(0.6);
+      for (var i = 0; i < smallCapacity; i++){ smallAirport.land(new Plane()); }
+      expect(function(){
+        smallAirport.land(plane)
+      }).toThrowError("We're packed")
+    })
   })
 });
--- jasmin/src/Airport.js
+++ jasmin/src/Airport.js
@@ -1,13 +1,24 @@
-function Airport() {
+function Airport(capacity = 10) {
   this.planes = []
+  this.weather = new Weather()
+  this.capacity = capacity
 }
 
 Airport.prototype.land = function(plane) {
+  if(this.weather.stormy()){
+    throw new Error('Too stormy!')
+  }
+  if(this.planes.length >= this.capacity){
+    throw new Error("We're packed")
+  }
   plane.land();
   this.planes.push(plane);
 }
 
 Airport.prototype.takeoff = function(plane) {
+  if(this.weather.stormy()){
+    throw new Error('Too stormy!')
+  }
   plane.takeoff();
   this.planes.shift(plane);
 }
